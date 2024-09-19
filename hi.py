619eef4bbf51e9c1c579aaff66f1743ce4f31363
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-09-20 00:39:38 +0530

    python in terminal

--- hi.py
+++ hi.py
@@ -19,3 +19,69 @@ print("this is my first phython code")
 from helo import chai
 
 chai("cbidiya")
+
+
+
+'''PS D:\python\PYTHON AGAIN>
+
+
+
+
+
+hiihi
+hiihi
+this is my first phython code
+hi
+5
+77
+PS D:\python\PYTHON AGAIN> python -u "d:\python\PYTHON AGAIN\hi.py"
+hiihi
+this is my first phython code
+hi
+5
+cbidiya
+PS D:\python\PYTHON AGAIN> python
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> hi
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'hi' is not defined
+>>> 2*5
+10
+>>> 3+5
+8
+>>> "chai"*4
+'chaichaichaichai'
+>>> import os
+>>> os.getcwd()
+'D:\\python\\PYTHON AGAIN'
+>>> for c in "chai":
+...     print(c)
+...
+c
+h
+a
+i
+>>> import sys
+>>> sys.platform
+'win32'
+>>> import helo
+hi
+5
+>>> chai('hi')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'chai' is not defined
+>>> chai(5)   
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'chai' is not defined
+>>> helo.chai('jii
+  File "<stdin>", line 1
+    helo.chai('jii
+              ^
+SyntaxError: unterminated string literal (detected at line 1)    
+>>> helo.chai("huuu")
+huuu
+>>> ^A'''
